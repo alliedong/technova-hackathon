2fa57319c42f851c45cead116e465192faeebe5b
Allie Dong <allied9420@gmail.com>
2021-08-29 01:14:53 -0400

    Co-authored-by: ashmitamittal <ashmitamittal@users.noreply.github.com> Co-authored-by: Dhriti Gabani <dgabani@uwaterloo.ca> Co-authored-by: Sarah Abellera <SGAbellera@users.noreply.github.com>

--- js/background.js
+++ js/background.js
@@ -1,27 +1,31 @@
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.local.get(["blocked", "enabled"], function (local) {
-      if (!Array.isArray(local.blocked)) {
-        chrome.storage.local.set({ blocked: [] });
-      }
-  
-      if (typeof local.enabled !== "boolean") {
-        chrome.storage.local.set({ enabled: false });
-      }
-    });
-  });
-  
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-    const url = changeInfo.pendingUrl || changeInfo.url;
-    if (!url || !url.startsWith("http")) {
-      return;
+  chrome.storage.local.get(["blocked", "enabled"], function (local) {
+    if (!Array.isArray(local.blocked)) {
+      chrome.storage.local.set({ blocked: [] });
     }
-  
-    const site = new URL(url).hostname;
-  
-    chrome.storage.local.get(["blocked", "enabled"], function (local) {
-      const { blocked, enabled } = local;
-      if (Array.isArray(blocked) && enabled && blocked.find(domain => site.includes(domain))) {
-        chrome.tabs.remove(tabId);
-      }
-    });
+
+    if (typeof local.enabled !== "boolean") {
+      chrome.storage.local.set({ enabled: false });
+    }
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+  const url = changeInfo.pendingUrl || changeInfo.url;
+  if (!url || !url.startsWith("http")) {
+    return;
+  }
+
+  const site = new URL(url).hostname;
+
+  chrome.storage.local.get(["blocked", "enabled"], function (local) {
+    const { blocked, enabled } = local;
+    if (
+      Array.isArray(blocked) &&
+      enabled &&
+      blocked.find((domain) => site.includes(domain))
+    ) {
+      chrome.tabs.remove(tabId);
+    }
+  });
 });
